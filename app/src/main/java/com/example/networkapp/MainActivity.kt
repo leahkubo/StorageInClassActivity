1f4439a89b30996d20c8ddc259554e6a882f3b59
leahkubo <kubalaleah@gmail.com>
2024-04-11 11:49:47 -0400

    Fixed Bugs and added SaveComic functionality

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.networkapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,9 +15,11 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
 
-// TODO (1: Fix any bugs)
-// TODO (2: Add function saveComic(...) to save and load comic info automatically when app starts)
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +29,18 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private lateinit var preferences: SharedPreferences
+    private val filename = "my_file"
+    private lateinit var file: File
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        preferences = getPreferences(MODE_PRIVATE)
+
+        file = File(filesDir, filename)
 
         requestQueue = Volley.newRequestQueue(this)
 
@@ -38,6 +49,22 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+
+        if (file.exists()){
+            try {
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line: String?
+                while (br.readLine().also { line = it } != null) {
+                    text.append(line)
+                    text.append('\n')
+                }
+                br.close()
+                downloadComic(text.toString())
+            } catch (e: java.lang.Exception){
+                e.printStackTrace()
+            }
+        }
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
@@ -51,6 +78,17 @@ class MainActivity : AppCompatActivity() {
             JsonObjectRequest(url, {showComic(it)}, {
             })
         )
+        saveComic(comicId)
+    }
+
+    private fun saveComic(comicId : String){
+        try{
+            val outputStream = FileOutputStream(file)
+            outputStream.write(comicId.toByteArray())
+            outputStream.close()
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 
     private fun showComic (comicObject: JSONObject) {
